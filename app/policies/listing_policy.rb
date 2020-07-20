df045b012d57c30a1c325b70ec7d338b4e0a79db
Alexander <alexander@Alex.local>
2020-07-20 18:33:27 +0700

    added div with overlay to banner

--- app/policies/listing_policy.rb
+++ app/policies/listing_policy.rb
@@ -8,5 +8,15 @@ class ListingPolicy < ApplicationPolicy
     return true
   end
 
+  def update?
+    record.user == user
+    # - record: the restaurant passed to the `authorize` method in controller
+    # - user:   the `current_user` signed in with Devise.
+  end
+
+  def destroy?
+    record.user == user
+  end
+
   end
 end
